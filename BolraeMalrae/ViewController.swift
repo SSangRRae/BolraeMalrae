8e5868f58851c5868ece9dd354ca1c7f7374faad
SSangRRae <pepe0003@kakao.com>
2024-02-02 20:36:47 +0900

    Initial Commit

---
+++ BolraeMalrae/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BolraeMalrae
+//
+//  Created by SangRae Kim on 2/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
